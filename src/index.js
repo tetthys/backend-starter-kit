de546b37b849683aee21e01e26674b690f2fbc96
tetthys <jeongwoo0830z@gmail.com>
2024-03-09 02:07:42 +0900

    Add basic middlewares

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import env from "./helper/env/env";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   return res.json({
     message: "Hello World!",
