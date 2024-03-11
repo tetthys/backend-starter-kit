dab7fe49187e699018b0efaeaafa189cd9117756
tetthys <jeongwoo0830z@gmail.com>
2024-03-11 19:46:38 +0900

    remove unnecessary imports

--- tests/tool/Cache/redis.test.js
+++ tests/tool/Cache/redis.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { describe, it, expect } from "vitest";
 import { createClient } from "redis";
 import delay from "../../../src/helper/delay/delay";
 import getUniqueString from "./utils/getUniqueString";
