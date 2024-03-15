a584db47fc5ddee12e257cdec46c96b122ee69e7
tetthys <jeongwoo0830z@gmail.com>
2024-03-15 21:40:55 +0900

    update

--- src/tool/Cache/Cache.js
+++ src/tool/Cache/Cache.js
@@ -1,31 +1,29 @@
-import { createClient } from "redis";
+import { Redis } from "ioredis";
 
 export default class Cache {
+  #redis = new Redis();
   #for;
 
   async set(key, value) {
-    const redis = await createClient().connect();
-
     switch (this.#for) {
       case "forOneMinute":
-        await redis.setEx(key, 60, JSON.stringify(value));
+        await this.#redis.setex(key, 60, JSON.stringify(value));
         break;
       case "forOneHour":
-        await redis.setEx(key, 60 * 60, JSON.stringify(value));
+        await this.#redis.setex(key, 60 * 60, JSON.stringify(value));
         break;
       case "forOneDay":
-        await redis.setEx(key, 60 * 60 * 24, JSON.stringify(value));
+        await this.#redis.setex(key, 60 * 60 * 24, JSON.stringify(value));
         break;
       default:
-        await redis.set(key, JSON.stringify(value));
+        await this.#redis.set(key, JSON.stringify(value));
     }
 
     return value;
   }
 
   async get(key, callback = undefined) {
-    const redis = await createClient().connect();
-    const value = await redis.get(key);
+    const value = await this.#redis.get(key);
     return value === null && callback ? await callback() : JSON.parse(value);
   }
 
