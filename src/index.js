d933872ea5cac1585311f55bc502d0f1ae16984c
tetthys <jeongwoo0830z@gmail.com>
2024-06-24 16:24:39 +0900

    set up nodemon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import env from "./Helpers/env/env";
 
 const app = express();
+const port = env("PORT");
 
 app.get("/hello", (req, res) => {
   res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
