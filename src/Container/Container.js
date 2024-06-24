b8bdcbb3b960d189894aced8bdeaec0630de8149
tetthys <jeongwoo0830z@gmail.com>
2024-06-24 15:08:00 +0900

    feat: Support bindToSingletone in nested injection

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,11 +1,31 @@
 export default class Container {
   static bindingMethods = [];
+  static singletones = [];
 
   static bind(className, bindingMethod) {
     this.bindingMethods.push({
       className: className,
       bindingMethod: bindingMethod,
     });
+  }
+
+  static bindToSingletone(className, bindingMethod) {
+    this.singletones.push({
+      className: className,
+      instance: bindingMethod(),
+    });
+  }
+
+  static isSingletone(className) {
+    return this.singletones.find(
+      (singletone) => singletone.className === className
+    );
+  }
+
+  static resolveSingletone(className) {
+    return this.singletones.find(
+      (singletone) => singletone.className === className
+    ).instance;
   }
 
   static hasBindingMethod(className) {
@@ -21,13 +41,15 @@ export default class Container {
   }
 
   static resolve(className) {
-    if (this.hasBindingMethod(className))
-      return this.resolveWithBindingMethod(className);
+    if (this.hasBindingMethod(className)) return this.resolveWithBindingMethod(className);
+    if (this.isSingletone(className)) return this.resolveSingletone(className);
+
     return className.injectables?.length > 0
       ? new className(
           ...className.injectables.map((injectable) => {
-            if (this.hasBindingMethod(injectable))
-              return this.resolveWithBindingMethod(injectable);
+            if (this.hasBindingMethod(injectable)) return this.resolveWithBindingMethod(injectable);
+            if (this.isSingletone(injectable)) return this.resolveSingletone(injectable);
+            
             return injectable.injectables?.length > 0
               ? Container.resolve(injectable)
               : new injectable();
--- tests/Container/Container.test.js
+++ tests/Container/Container.test.js
@@ -1,4 +1,4 @@
-import { expect } from "vitest";
+import { beforeEach, expect } from "vitest";
 import Container from "../../src/Container/Container";
 import Head from "../../src/Stubs/Head";
 import Eye from "../../src/Stubs/Eye";
@@ -7,6 +7,11 @@ import Mouth from "../../src/Stubs/Mouth";
 import Teeth from "../../src/Stubs/Teeth";
 
 describe("Container", () => {
+  beforeEach(() => {
+    Container.bindingMethods = [];
+    Container.singletones = [];
+  });
+
   it("returns Head instance with correct members", () => {
     const head = Container.resolve(Head);
     expect(head.eye).toBeInstanceOf(Eye);
@@ -32,5 +37,23 @@ describe("Container", () => {
     expect(head.nose).toBeInstanceOf(Eye);
     expect(head.mouth).toBeInstanceOf(Mouth);
     expect(head.mouth.teeth).toBeInstanceOf(Teeth);
+  });
+
+  it("binds to singletone", () => {
+    Container.bindToSingletone(Nose, () => {
+      return new Nose();
+    });
+    const nose1 = Container.resolve(Nose);
+    const nose2 = Container.resolve(Nose);
+    expect(nose1).toBe(nose2);
+  });
+
+  it("binds to singletone in nested injection", () => {
+    Container.bindToSingletone(Nose, () => {
+      return new Nose();
+    });
+    const head1 = Container.resolve(Head);
+    const head2 = Container.resolve(Head);
+    expect(head1.nose).toBe(head2.nose);
   });
 });
